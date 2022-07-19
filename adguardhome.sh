4a31570d685c657e99c092ca5ab3bc9f40a7439f
YY-China <43245993+YY-China@users.noreply.github.com>
2022-07-19 10:44:37 +0800

    Update adguardhome.sh

--- adguardhome.sh
+++ adguardhome.sh
@@ -253,32 +253,31 @@ fi
 
 dl_adg(){
 logger -t "AdGuardHome" "下载AdGuardHome"
-curl -k -s -o /tmp/AdGuardHome/AdGuardHome --connect-timeout 10 --retry 3 https://ghproxy.com/https://raw.githubusercontent.com/YY-China/Adguardhome-Config/main/AdGuardHome
-if [ ! -f "/tmp/AdGuardHome/AdGuardHome" ]; then
+curl -k -s -o /media/AiDisk_a1/AdGuardHome --connect-timeout 10 --retry 3 https://ghproxy.com/https://raw.githubusercontent.com/YY-China/Adguardhome-Config/main/AdGuardHome
+if [ ! -f "/media/AiDisk_a1/AdGuardHome" ]; then
 logger -t "AdGuardHome" "AdGuardHome下载失败，请检查是否能正常访问github!程序将退出。"
 nvram set adg_enable=0
 exit 0
 else
 logger -t "AdGuardHome" "AdGuardHome下载成功。"
-chmod 777 /tmp/AdGuardHome/AdGuardHome
+chmod 777 /media/AiDisk_a1/AdGuardHome
 fi
 }
 
 start_adg(){
-    mkdir -p /tmp/AdGuardHome
 	mkdir -p /etc/storage/AdGuardHome
-	if [ ! -f "/tmp/AdGuardHome/AdGuardHome" ]; then
+	if [ ! -f "/media/AiDisk_a1/AdGuardHome" ]; then
 	dl_adg
 	fi
 	getconfig
 	change_dns
 	set_iptable
 	logger -t "AdGuardHome" "运行AdGuardHome"
-	eval "/tmp/AdGuardHome/AdGuardHome -c $adg_file -w /tmp/AdGuardHome -v" &
+	eval "/media/AiDisk_a1/AdGuardHome -c $adg_file -w /media/AiDisk_a1 -v" &
 
 }
 stop_adg(){
-rm -rf /tmp/AdGuardHome
+#rm -rf /tmp/AdGuardHome
 killall -9 AdGuardHome
 del_dns
 clear_iptable
