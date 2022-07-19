f8325c42dc8a50b989d5f5156602d50cf5d75eda
YY-China <43245993+YY-China@users.noreply.github.com>
2022-07-19 21:59:44 +0800

    Update adguardhome.sh

--- adguardhome.sh
+++ adguardhome.sh
@@ -66,12 +66,11 @@ bind_port: 3030
 beta_bind_port: 0
 users:
 - name: xiaoyangzai
-  password: $2a$10$d3Xsh/Yu1WD6F/QKQLlLZukGworGUHITxUHIUAmAlrNgNJiTMwQpa
-auth_attempts: 3
-block_auth_min: 10
+  password: $2a$10$Z3uQ9O8/FH1niemCGWFdI.egeqnkciOj8VdC8UHvuK0Sf6X7bXnv6
+auth_attempts: 5
+block_auth_min: 15
 http_proxy: ""
 language: ""
-rlimit_nofile: 0
 debug_pprof: false
 web_session_ttl: 600
 dns:
@@ -81,17 +80,17 @@ dns:
   statistics_interval: 1
   querylog_enabled: true
   querylog_file_enabled: true
-  querylog_interval: 1
+  querylog_interval: 24h
   querylog_size_memory: 1000
   anonymize_client_ip: false
   protection_enabled: true
   blocking_mode: default
   blocking_ipv4: ""
   blocking_ipv6: ""
-  blocked_response_ttl: 60
+  blocked_response_ttl: 10
   parental_block_host: family-block.dns.adguard.com
   safebrowsing_block_host: standard-block.dns.adguard.com
-  ratelimit: 0
+  ratelimit: 20
   ratelimit_whitelist: []
   refuse_any: true
   upstream_dns:
@@ -104,19 +103,23 @@ dns:
   - https://doh.360.cn/dns-query
   upstream_dns_file: ""
   bootstrap_dns:
-  - 180.76.76.76
-  - 114.114.114.114
-  all_servers: true
+  - 114.114.115.115
+  all_servers: false
   fastest_addr: false
+  fastest_timeout: 1s
   allowed_clients: []
   disallowed_clients: []
   blocked_hosts:
   - version.bind
   - id.server
   - hostname.bind
+  trusted_proxies:
+  - 127.0.0.0/8
+  - ::1/128
   cache_size: 4194304
   cache_ttl_min: 600
   cache_ttl_max: 86400
+  cache_optimistic: true
   bogus_nxdomain: []
   aaaa_disabled: true
   enable_dnssec: false
@@ -134,8 +137,9 @@ dns:
   cache_time: 30
   rewrites: []
   blocked_services: []
-  local_domain_name: lan
-  resolve_clients: true
+  upstream_timeout: 10s
+  private_networks: []
+  use_private_ptr_resolvers: true
   local_ptr_upstreams: []
 tls:
   enabled: false
@@ -143,7 +147,7 @@ tls:
   force_https: false
   port_https: 443
   port_dns_over_tls: 853
-  port_dns_over_quic: 784
+  port_dns_over_quic: 853
   port_dnscrypt: 0
   dnscrypt_config_file: ""
   allow_unencrypted_doh: false
@@ -161,68 +165,68 @@ filters:
   url: https://adaway.org/hosts.txt
   name: AdAway Default Blocklist
   id: 2
-- enabled: false
-  url: https://www.malwaredomainlist.com/hostslist/hosts.txt
-  name: MalwareDomainList.com Hosts List
-  id: 4
-- enabled: true
-  url: https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/hosts.txt
-  name: NoCoin Filter List
-  id: 1657854228
 - enabled: true
   url: https://someonewhocares.org/hosts/zero/hosts
   name: Dan Pollock's List
-  id: 1657854229
-- enabled: true
-  url: https://pgl.yoyo.org/adservers/serverlist.php?hostformat=adblockplus&showintro=1&mimetype=plaintext
-  name: Peter Lowe's List
-  id: 1657854230
-- enabled: true
-  url: https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/SmartTV-AGH.txt
-  name: Perflyst and Dandelion Sprout's Smart-TV Blocklist
-  id: 1657854231
-- enabled: true
-  url: https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt
-  name: WindowsSpyBlocker - Hosts spy rules
-  id: 1657854232
+  id: 1658238573
 - enabled: true
   url: https://raw.githubusercontent.com/DandelionSprout/adfilt/master/GameConsoleAdblockList.txt
   name: Game Console Adblock List
-  id: 1657854233
+  id: 1658238574
+- enabled: true
+  url: https://abp.oisd.nl/basic/
+  name: OISD Blocklist Basic
+  id: 1658238575
+- enabled: true
+  url: https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/SmartTV-AGH.txt
+  name: Perflyst and Dandelion Sprout's Smart-TV Blocklist
+  id: 1658238576
+- enabled: true
+  url: https://pgl.yoyo.org/adservers/serverlist.php?hostformat=adblockplus&showintro=1&mimetype=plaintext
+  name: Peter Lowe's List
+  id: 1658238577
+- enabled: true
+  url: https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareAdGuardHome.txt
+  name: Dandelion Sprout's Anti-Malware List
+  id: 1658238578
+- enabled: true
+  url: https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/hosts.txt
+  name: NoCoin Filter List
+  id: 1658238579
 - enabled: true
   url: https://raw.githubusercontent.com/durablenapkin/scamblocklist/master/adguard.txt
   name: Scam Blocklist by DurableNapkin
-  id: 1657854234
-- enabled: true
-  url: https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt
-  name: Spam404
-  id: 1657854235
+  id: 1658238580
 - enabled: true
   url: https://raw.githubusercontent.com/mitchellkrogza/The-Big-List-of-Hacked-Malware-Web-Sites/master/hosts
   name: The Big List of Hacked Malware Web Sites
-  id: 1657854236
+  id: 1658238581
+- enabled: true
+  url: https://malware-filter.gitlab.io/malware-filter/urlhaus-filter-agh-online.txt
+  name: Online Malicious URL Blocklist
+  id: 1658238582
 - enabled: true
   url: https://anti-ad.net/easylist.txt
   name: 'CHN: anti-AD'
-  id: 1657854238
+  id: 1658238583
 - enabled: true
-  url: https://paulgb.github.io/BarbBlock/blacklists/hosts-file.txt
-  name: BarbBlock
-  id: 1657854239
+  url: https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt
+  name: WindowsSpyBlocker - Hosts spy rules
+  id: 1658238584
 whitelist_filters:
 - enabled: true
   url: https://fastly.jsdelivr.net/gh/privacy-protection-tools/dead-horse@master/anti-ad-white-list.txt
   name: anti-AD (Whitelist)
-  id: 1657854243
+  id: 1658238585
 user_rules:
 - '||_vlmcs._tcp.lan^$dnsrewrite=NOERROR;SRV;0 100 1688 my.router'
 - '||app.drivereasy.com'
 - '@@||datayi.cn^$important'
 - '@@||launches.appsflyer.com^$important'
-- ""
 dhcp:
   enabled: false
   interface_name: ""
+  local_domain_name: lan
   dhcpv4:
     gateway_ip: ""
     subnet_mask: ""
@@ -236,7 +240,14 @@ dhcp:
     lease_duration: 86400
     ra_slaac_only: false
     ra_allow_slaac: false
-clients: []
+clients:
+  runtime_sources:
+    whois: true
+    arp: true
+    rdns: true
+    dhcp: true
+    hosts: true
+  persistent: []
 log_compress: false
 log_localtime: false
 log_max_backups: 0
@@ -244,7 +255,12 @@ log_max_size: 100
 log_max_age: 3
 log_file: ""
 verbose: false
-schema_version: 10
+os:
+  group: ""
+  user: ""
+  rlimit_nofile: 0
+schema_version: 14
+
 
 EEE
 	chmod 755 "$adg_file"
